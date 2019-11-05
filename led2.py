3ec31bdc65c71d07efb5867bb89a8ed9b128dab5
trenerise <39572451+trenerise@users.noreply.github.com>
2019-11-05 08:56:36 +0200

    v2

--- led2.py
+++ led2.py
@@ -3,8 +3,6 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18,GPIO.OUT)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(27,GPIO.OUT)
 
 counter = 0
 try:
@@ -12,16 +10,8 @@ try:
 		print "LED 1  on"
 		GPIO.output(18,GPIO.HIGH)
 		time.sleep(1)
-		print "LED 2  on"
-		GPIO.output(17,GPIO.HIGH)
-		time.sleep(1)
-		print "LED 3 on"
-		GPIO.output(27,GPIO.HIGH)
-		time.sleep(3)
 		print "LED off"
 		GPIO.output(18,GPIO.LOW)
-		GPIO.output(17,GPIO.LOW)
-		GPIO.output(27,GPIO.LOW)
 		counter = counter + 1
 except KeyboardInterrupt:
 	print "\n", counter
